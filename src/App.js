09afef70efef182ba14b493a1f3b8261c4d2dc32
dhyeymistri <73789091+dhyeymistri@users.noreply.github.com>
2021-11-30 19:37:00 +0530

    vid 16

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-// import logo from './logo.svg';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar'
 import TextArea from './components/TextArea'
 import React, { useState } from 'react'
 import Alert from './components/Alert';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setmode] = useState('light')
   const [alert, setalert] = useState(null)
-  const toggleMode = () =>{
-    if(mode === "light"){
+  const toggleMode = () => {
+    if (mode === "light") {
       setmode("dark")
-      document.body.style.backgroundColor='black'
+      document.body.style.backgroundColor = 'black'
       showAlert("Dark Mode has been enabled!", "success")
     }
-    else{
+    else {
       setmode("light")
-      document.body.style.backgroundColor='white'
+      document.body.style.backgroundColor = 'white'
       showAlert("Light Mode has been enabled!", "success")
     }
   }
-  const showAlert = (message, type) =>{
+  const showAlert = (message, type) => {
     setalert({
       msg: message,
       type: type
@@ -33,12 +36,18 @@ function App() {
   }
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert} />
-      <div className="container">
-      <TextArea showAlert={showAlert} heading="Enter your text below" mode={mode}/>
-      {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container">
+          <div className="container my-3">
+            <Routes>
+              <Route path="/about" element={<About/>}/>
+              <Route path="/" element={<TextArea showAlert={showAlert} heading="Enter your text below" mode={mode} />}/>
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </>
   );
 }
